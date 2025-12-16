206867a41f46ed809511d00fae207056a5fb2b90
johnkentm6-afk <johnkentm6@gmail.com>
2025-12-16 21:49:06 +0800

    Update fyt2.js

--- raza/commands/fyt2.js
+++ raza/commands/fyt2.js
@@ -1,7 +1,7 @@
 module.exports.config = {
     name: "fyt2",
     version: "1.0.0",
-    hasPermssion: 2,
+    hasPermission: 2,
     credits: "... - Long LTD",
     description: "Rage Mode",
     commandCategory: "group",
@@ -13,406 +13,60 @@ module.exports.config = {
     }
 }
 
-module.exports.run = async function({ api, args, Users, event}) {
-  var say = args.join(" ")
-   var n = say
-  let r = 10000;
-  
-
- //let diff = 400;
-// for (let i = 0; i < 10; i++) {
-
-  
-  var a = function (a) { 
-      
-      api.sendMessage(a, event.threadID); }
-a(`${n} antaba mo`);
-
-setTimeout(() => {a({body: `${n} papalag kaba`})}, r);
-
-  
-setTimeout(() => {a({body: `${n} batokbatokan kita` })}, 2*r);
-
-
-setTimeout(() => {a({body: `${n} taba mo`})} , 3*r);
-
-
-
-setTimeout(() => {a({body: `${n} wala ka mama`})} , 4*r);
-
-
-
-setTimeout(() => {a({body: `${n} oo papa ma namatay dahil sa konat ko ` })}, 5*r);
-
-
-setTimeout(() => {a({body: `${n} nanay mo kinantot ko habang naka tingin ka` })}, 6*r);
-
-
-setTimeout(() => {a({body: `${n} bading amp hahaa`})}, 7*r);
-
-
-setTimeout(() => {a({body: `${n} nanay mo shockla`})}, 8*r);
-
-
-setTimeout(() => {a({body: `${n} bading ka nga ?`})}, 9*r);
-
-
-setTimeout(() => {a({body: `${n} gooding🤣🤣🤣`})}, 10*r);
-
-
-setTimeout(() => {a({body: `${n} hwhahahahaa`})}, 11*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 1`})}, 12*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 2`})}, 13*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 3`})}, 14*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 4`})}, 15*r);
-
-  
-setTimeout(() => {a({body: `${n} */silent 5` })}, 16*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 7`})} , 17*r);
-
-
-
-setTimeout(() => {a({body: `${n} */silent 8`})} , 18*r);
-
-
-
-setTimeout(() => {a({body: `${n} */silent 9` })}, 19*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 10` })}, 20*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 11`})}, 21*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 12`})}, 22*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 13`})}, 23*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 14`})}, 24*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 15`})}, 25*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 16`})}, 26*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 17`})}, 27*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 18`})}, 28*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 19`})}, 29*r);
-
-  
-setTimeout(() => {a({body: `${n} */silent 20` })}, 30*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 21`})} , 31*r);
-
-
-
-setTimeout(() => {a({body: `${n} */silent 22`})} , 32*r);
-
-
-
-setTimeout(() => {a({body: `${n} */silent 23` })}, 33*r);
-
-setTimeout(() => {a({body: `${n} */silent 24` })}, 34*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 25`})}, 35*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 26`})}, 36*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 27`})}, 37*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 28`})}, 38*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 29`})}, 39*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 30`})}, 40*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 31`})}, 41*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 32`})}, 42*r);
-
-
-
-setTimeout(() => {a({body: `${n} */silent 33`})}, 43*r);
-
-  
-setTimeout(() => {a({body: `${n} */silent 34` })}, 44*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 35`})} , 45*r);
-
-
-
-setTimeout(() => {a({body: `${n} */silent 36`})} , 46*r);
-
-
-
-setTimeout(() => {a({body: `${n} */silent 37` })}, 47*r);
-
-
- setTimeout(() => {a({body: `${n} */silent 38` })}, 48*r);
-
-
-  setTimeout(() => {a({body: `${n} */silent 39`})}, 49*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 40`})}, 50*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 41`})}, 51*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 42`})}, 52*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 43`})}, 53*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 44`})}, 54*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 45`})}, 55*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 46`})}, 56*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 48`})}, 57*r);
-
-  
-setTimeout(() => {a({body: `${n} */silent 49` })}, 58*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 50`})} , 59*r);
-
-
-
-setTimeout(() => {a({body: `${n} */silent 51`})} , 60*r);
-
-
-
-setTimeout(() => {a({body: `${n} */silent 52` })}, 61*r);
-
-
- setTimeout(() => {a({body: `${n} */silent 53` })}, 62*r);
-
-
-  setTimeout(() => {a({body: `${n} */silent 54`})}, 63*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 55`})}, 64*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 56`})}, 65*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 57`})}, 66*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 58`})}, 67*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 59`})}, 68*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 60`})}, 69*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 61`})}, 70*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 62`})}, 71*r);
-
-  
-
- // setTimeout(() => {a({body: "NIKAL MADHARCHOD RANDI K PILLE"})} , 1500);
-
-
-
-
-  
-
-
-
-
-
-
-
-  
-
-
-
-
-
-  
-
-
-
-
-
-
-  
-  setInterval(() => { 
-    var a = function (a) { 
-      
-      api.sendMessage(a, event.threadID); }
-a(`${n} KUTTIIAAA K PILLE K BACHHEE BSDDKK`);
-
-setTimeout(() => {a({body: `${n} */silent 61`})}, 1*r);
-
-  
-setTimeout(() => {a({body: `${n} */silent 62` })}, 2*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 64`})} , 3*r);
-
-
-
-setTimeout(() => {a({body: `${n} */silent 65`})} , 4*r);
-
-
-
-setTimeout(() => {a({body: `${n} */silent 66` })}, 5*r);
-
-
- setTimeout(() => {a({body: `${n} */silent 67` })}, 6*r);
-
-
-  setTimeout(() => {a({body: `${n} */silent 68`})}, 7*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 69`})}, 8*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 70`})}, 9*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 71`})}, 10*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 72`})}, 11*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 73`})}, 12*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 74`})}, 13*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 75`})}, 14*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 76`})}, 15*r);
-
-  
-setTimeout(() => {a({body: `${n} */silent 77` })}, 16*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 78`})} , 17*r);
-
-
-
-setTimeout(() => {a({body: `${n} */silent 79`})} , 18*r);
-
-
-
-setTimeout(() => {a({body: `${n} */silent 80` })}, 19*r);
-
-
- setTimeout(() => {a({body: `${n} */silent 81` })}, 20*r);
-
-
-  setTimeout(() => {a({body: `${n} */silent 82`})}, 21*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 83`})}, 22*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 84`})}, 23*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 85`})}, 24*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 86`})}, 25*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 87`})}, 26*r);
-
-
-setTimeout(() => {a({body: `${n} T*/silent 88`})}, 27*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 89`})}, 28*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 90`})}, 29*r);
-
-  
-setTimeout(() => {a({body: `${n} */silent 91` })}, 30*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 92`})} , 31*r);
-
-
-
-setTimeout(() => {a({body: `${n}*/silent 93`})} , 32*r);
-
-
-
-setTimeout(() => {a({body: `${n} */silent 94` })}, 33*r);
-
-
- setTimeout(() => {a({body: `${n} */silent 95` })}, 34*r);
-
-
-  setTimeout(() => {a({body: `${n} */silent 96`})}, 35*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 97`})}, 36*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 98`})}, 37*r);
-
-
-setTimeout(() => {a({body: `${n} */silent 99`})}, 38*r);
-
-
-setTimeout(() => {a({body: `${n} T*/silent 100`})}, 39*r);
-
-
- // setTimeout(() => {a({body: "NIKAL MADHARCHOD RANDI K PILLE"})} , 1500);
-
-
-  } , 40*r);
-
-  
- // }
-                     }
+module.exports.run = async function({ api, args, Users, event }) {
+    var say = args.join(" ");
+    var n = say;
+    let r = 7000;  // Set delay to 7 seconds (7000 ms)
+
+    // Function to send a message to the thread
+    var sendMessage = function(msg) { 
+        api.sendMessage(msg, event.threadID); 
+    };
+
+    // List of messages to be sent
+    let messages = [
+        `${n} antaba mo`,
+        `${n} papalag kaba`,
+        `${n} batokbatokan kita`,
+        `${n} taba mo`,
+        `${n} wala ka mama`,
+        `${n} oo papa ma namatay dahil sa konat ko`,
+        `${n} nanay mo kinantot ko habang naka tingin ka`,
+        `${n} bading amp hahaa`,
+        `${n} nanay mo shockla`,
+        `${n} bading ka nga?`,
+        `${n} gooding🤣🤣🤣`,
+        `${n} hwhahahahaa`,
+        `${n} */silent 1`,
+        `${n} */silent 2`,
+        `${n} */silent 3`,
+        `${n} */silent 4`,
+        `${n} */silent 5`,
+        `${n} */silent 6`,
+        `${n} */silent 7`,
+        `${n} */silent 8`,
+        `${n} */silent 9`,
+        `${n} */silent 10`
+    ];
+
+    // Send messages one by one
+    let index = 0;
+    let interval = setInterval(() => {
+        if (index < messages.length) {
+            sendMessage(messages[index]);
+            index++;
+        } else {
+            clearInterval(interval);  // Stop the interval after all messages are sent
+        }
+    }, r);  // Set interval to 7 seconds (7000ms)
+
+    // Optionally, we can also allow the user to turn off the interval manually
+    // For example, if you want to stop the interval on another command or condition
+    // you can use clearInterval(interval);
+    
+    // Example: If another argument `stop` is passed, clear the interval
+    if (args.includes('stop')) {
+        clearInterval(interval);
+        sendMessage(`${n} The rage mode has been stopped.`);
+    }
+};
