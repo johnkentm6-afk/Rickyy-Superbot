4c3ea250881cb7c9e2a1d345ed9c09883360425d
johnkentm6-afk <johnkentm6@gmail.com>
2025-12-29 20:43:13 +0800

    Update oras.js

--- raza/commands/oras.js
+++ raza/commands/oras.js
@@ -3,7 +3,7 @@ const moment = require("moment-timezone");
 module.exports = {
   config: {
     name: "oras",
-    aliases: ["ilangoras", "ilang-oras-kana-sa-gc"],
+    aliases: ["ilang oras kana sa gc", "ilang-oras-kana-sa-lgc"],
     version: "1.0.5",
     role: 0,
     author: "Rickyy / Gemini",
